0a466204b4d9a33fa5aa9a0fec7ea4087d9a03a1
kubs0135 <kubskathayat01@gmail.com>
2024-07-20 23:05:09 +0545

    learned about variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+//ES5
+
+var person = "raktim";
+
+//ES6
+const anotherPerson = "kk";
+let thirdPerson = "kuber";
+
+console.log(person, anotherPerson, thirdPerson);
+
+const testPerson = "anil";
+
+console.log(testPerson);
+
+let nextPerson = "anil";
+nextPerson = "kk";
+console.log(nextPerson);
+
+//Always start variables with const keyword
+const table = "red table";
+console.log(table);
+let phone = 9765409713;
+phone = 9803284872;
+console.log(phone);
