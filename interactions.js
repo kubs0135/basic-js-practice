69d4d427fc3ffcbbedd05c35696f8f11f04eeb7f
kubs0135 <kubskathayat01@gmail.com>
2024-07-20 23:24:41 +0545

    learned about interactions

---
+++ interactions.js
@@ -0,0 +1,16 @@
+//user interaction
+
+//alert
+// alert("hello world");
+
+//prompt
+const age = prompt("How old are you?");
+
+//confirm
+const above18 = confirm("Are you above 18 years old");
+
+console.log(age, above18);
+
+alert(`you are ${age} years old`); //string literal
+
+//write a js program to create sum calculator using user interaction
