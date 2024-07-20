87b41f94c3f0b7fd6bc10a414579aaadeefbedb5
kubs0135 <kubskathayat01@gmail.com>
2024-07-20 23:06:03 +0545

    learned about comments

---
+++ comments.js
@@ -0,0 +1,17 @@
+//single line comment
+//this is a single line comment
+
+//multi line commetn
+
+/* 
+Documentation or logic explains
+my
+names
+is
+kuber
+*/
+
+// this
+// is
+// another
+// Comment
