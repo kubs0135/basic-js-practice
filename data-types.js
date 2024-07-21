457463acac3aed0a2e6fda20ab341ad39093e814
kubs0135 <kubskathayat01@gmail.com>
2024-07-21 15:56:46 +0545

    learned about datatypes

---
+++ data-types.js
@@ -0,0 +1,8 @@
+//Data types(5)
+//1. Primitive Datatype
+//2.Complex Datatype
+//type conversion/coercion
+//const a="2";
+// console.log(typesof a);
+// const num=Number(a);// type conversion
+// console.log(num);
