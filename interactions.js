b497d3a953ecb5814bd4a3b26e2af8cfd6d49e26
kubs0135 <kubskathayat01@gmail.com>
2024-07-21 15:53:54 +0545

    completing sum calculator assignment

--- interactions.js
+++ interactions.js
@@ -4,13 +4,20 @@
 // alert("hello world");
 
 //prompt
-const age = prompt("How old are you?");
+// const age = prompt("How old are you?");
 
-//confirm
-const above18 = confirm("Are you above 18 years old");
+// //confirm
+// const above18 = confirm("Are you above 18 years old");
 
-console.log(age, above18);
+// console.log(age, above18);
 
-alert(`you are ${age} years old`); //string literal
+// alert(`you are ${age} years old`); //string literal
 
 //write a js program to create sum calculator using user interaction
+
+const num1 = prompt("ENter first number");
+console.log(typeof num1);
+const num2 = prompt("ENter second number");
+console.log({ num1, num2 });
+const sum = Number(num1) + Number(num2);
+alert(`The sum of number ${num1} and ${num2} is ${sum} `);
