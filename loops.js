d2f77fa36823ac4d43a77950f8f376ff1f385cd5
kubs0135 <kubskathayat01@gmail.com>
2024-07-21 17:34:18 +0545

    learned loops

---
+++ loops.js
@@ -0,0 +1,57 @@
+/*
+
+for 
+do while
+while
+
+*/
+
+//for (syntax)
+// for(begin;step condition;increase begin step){
+//     //code to be executed
+// }
+
+//write a js program for multiplication table
+
+// const table = Number(prompt("Enter the number for table ;2"));
+// if (isNaN(table)) {
+//   alert("you did not enter number");
+// }
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${table}*${i}=${table * i}`);
+// }
+
+//do while loop
+
+//begin step
+// do{
+//     //code execution
+//     //increase begin step
+// }while(condition check)
+
+// const table1 = Number(prompt("Enter the number for table ;2"));
+// if (isNaN(table1)) {
+//   alert("you did not enter number");
+// }
+let i = 1;
+// do {
+//  console.log(`${table1}*${i}=${table1 * i}`);
+//  i++;
+//  } while (i <= 10);
+
+// begin step
+// //while loop
+// while(condition){
+//     code execution
+//     increase begin step
+
+// }
+
+const table2 = Number(prompt("Enter the number for table3 ;2"));
+if (isNaN(table2)) {
+  alert("you did not enter number");
+}
+while (i <= 10) {
+  console.log(`${table2}*${i}=${table2 * i}`);
+  i++;
+}
