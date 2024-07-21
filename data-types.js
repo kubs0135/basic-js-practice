ec08d3b96044781c81680ac9d35daa039d46f284
kubs0135 <kubskathayat01@gmail.com>
2024-07-21 15:57:53 +0545

    updated data types document

--- data-types.js
+++ data-types.js
@@ -3,6 +3,6 @@
 //2.Complex Datatype
 //type conversion/coercion
 //const a="2";
-// console.log(typesof a);
+// console.log(typesof a); //check type
 // const num=Number(a);// type conversion
 // console.log(num);
