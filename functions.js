451e33741e80c034e9d6aa13b8e96430182b949c
kubs0135 <kubskathayat01@gmail.com>
2024-07-22 17:53:02 +0545

    learned all types of functions

--- functions.js
+++ functions.js
@@ -38,3 +38,143 @@ const sum = (a, b) => {
 //function call/execute
 const result = sum(2, 3);
 console.log({ result });
+
+/*
+Types of function
+1.Default function
+2.parameterized function
+3.arrow function
+4.closure
+5.IIFes
+6.recursive function
+7.inline function
+8.anonymous function
+9.callback function
+10.pure function
+11.implicit function
+12.explicit function
+
+
+*/
+
+//default
+
+const defaultFunc = (name1 = "user") => {
+  console.log(`Hello ${name1}`);
+};
+
+defaultFunc("Admin");
+defaultFunc();
+
+//parameterized function
+
+const sumation = ({ a, b, c, d }) => {
+  console.log(`Sum is:${a + b + c + d}`);
+};
+sumation({ a: 2, b: 3, d: 1, c: 1 });
+
+//Arrow Function
+
+const summations = (a, b) => {
+  return a + b;
+};
+
+//Closure Function
+
+const mainCounter = () => {
+  let count = 0;
+  return () => {
+    count = count + 1;
+    return count;
+  };
+};
+
+const count1 = mainCounter(); //dasjhain counter
+const count2 = mainCounter(); //new year counter
+
+console.log(count1());
+console.log(count1());
+console.log(count1());
+
+console.log(count2());
+console.log(count2());
+
+//5. IIFEs (Immediately Invoked Functional Expression)
+//DevOps engineer le use garxan(Database seeding)
+((namek) => {
+  console.log(`hello ${namek}`);
+})("kuber");
+
+//6.Recursive Function
+
+//factorial of 5
+
+const factorial = (num) => {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
+
+console.log(factorial(5));
+
+//write a recursion formula for countdown
+
+const countDown = (counter) => {
+  console.log(counter);
+  let counts = counter - 1;
+  if (counts === 0) return 0;
+  return countDown(counts);
+};
+
+countDown(10);
+
+//7.inline function
+const test = () => {
+  const d = 1;
+  console.log({ d });
+};
+
+const d = 5;
+
+function testss() {
+  console.log("global func", { d });
+}
+
+test();
+testss();
+//8.anonymous function
+(a) => {
+  console.log(a);
+};
+
+//9.callback function
+
+const print = (data) => {
+  console.log({ data });
+};
+
+const parent = (func) => {
+  const num = "kubewrrrrr";
+  func(num);
+};
+
+parent(print);
+
+//10. pure function
+const upperConverter = (text) => {
+  console.log(text.toUpperCase());
+};
+
+upperConverter("kuber kathayat");
+
+//11. implcit function
+
+const summm = (a, b) => a + b;
+console.log(summm(2, 4));
+
+//12. explicit function
+const summmmm = (a, b) => {
+  return a + b;
+};
+console.log(summmmm(5, 14));
